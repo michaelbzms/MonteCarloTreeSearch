02631a6ad0fccd8c0aee27cab9a36cbe443bd78b
Michael Bzms <35953365+michaelbzms@users.noreply.github.com>
2021-08-07 19:19:11 +0300

    WIP: Quoridor State

--- examples/Quoridor/Quoridor.cpp
+++ examples/Quoridor/Quoridor.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
 #include "Quoridor.h"
+
+
+using namespace std;
 
 
 Quoridor_state::Quoridor_state()
@@ -17,3 +21,115 @@ Quoridor_state::Quoridor_state(const Quoridor_state &other)
         wall_connections[i / 8][i % 8] = other.wall_connections[i / 8][i % 8];
     }
 }
+
+char Quoridor_state::check_winner() const {
+    if (wy == 8) return 'W';
+    if (by == 0) return 'B';
+    return ' ';
+}
+
+void Quoridor_state::add_wall(short int x, short int y, bool horizontal) {
+    char put = horizontal ? 'h' : 'v', other = horizontal ? 'v' : 'h';
+    if (walls[x][y] == ' ') walls[x][y] = put;
+    else if (walls[x][y] == other) walls[x][y] = 'b';
+    else {
+        cerr << "Warning: Illegal wall let through!" << endl;   // should not happen
+    }
+}
+
+bool Quoridor_state::legal_step(short int x, short int y, char p) const {
+    // TODO
+    return false;
+}
+
+bool Quoridor_state::legal_wall(short int x, short int y, char p, bool horizontal) const {
+    // TODO
+    return false;
+}
+
+bool Quoridor_state::legal_move(const Quoridor_move *move) const {
+    if (move == NULL) return false;
+    if (move->player != 'W' && move->player != 'B'){
+        cerr << "Warning: wrong player argument!" << endl;
+        return false;
+    }
+    if (move->type == 'h' || move->type == 'v') {   // wall move
+        return legal_wall(move->x, move->y, move->player, move->type == 'h');
+    } else {                                        // pawn move
+        return legal_step(move->x, move->y, move->player);
+    }
+}
+
+void Quoridor_state::play_move(const Quoridor_move *move) {
+    if (!legal_move(move)) {
+        cerr << "Attempted illegal move:" << move << endl;
+        return;
+    }
+    if (move->type == 'h' || move->type == 'v') {   // wall move
+        // play legal wall
+        add_wall(move->x, move->y, move->type == 'h');
+        if (move->type == 'h'){
+            add_wall(move->x, move->y + 1, true);
+        } else {
+            add_wall(move->x + 1, move->y, false);
+        }
+        wall_connections[move->x][move->y] = true;
+        // reduce walls
+        switch (move->player) {
+            case 'W':
+                wwallsno--;
+                break;
+            case 'B':
+                bwallsno--;
+                break;
+        }
+    } else {                                        // pawn move
+        // play legal move
+        switch (move->player) {
+            case 'W':
+                wx = move->x;
+                wy = move->y;
+                break;
+            case 'B':
+                bx = move->x;
+                by = move->y;
+                break;
+        }
+    }
+    // change turn
+    change_turn();
+}
+
+///////////////////////////////////////////////////////////////////////////
+
+bool Quoridor_state::is_terminal() const {
+    char winner = check_winner();
+    return winner != 'W' && winner != 'B';
+}
+
+MCTS_state *Quoridor_state::next_state(const MCTS_move *move) const {
+    Quoridor_state *new_state = new Quoridor_state(*this);
+    new_state->play_move((const Quoridor_move *) move);
+    return new_state;
+}
+
+/** It is very important to decide which actions we will be considering.
+ *  We would like to only consider good moves by using appropriate heuristics.
+ *  This minimizes the branching factor of the search tree while also not investing
+ *  in subtrees caused by bad enemy (and also ours) moves, where we would probably be better anyway.
+ */
+queue<MCTS_move *> *Quoridor_state::actions_to_try() const {
+    // TODO
+    return nullptr;
+}
+
+double Quoridor_state::rollout() const {
+    // TODO
+    return 0;
+}
+
+void Quoridor_state::print() const {
+    // TODO
+    MCTS_state::print();
+}
+
--- examples/Quoridor/Quoridor.h
+++ examples/Quoridor/Quoridor.h
@@ -4,24 +4,50 @@
 #include "../../mcts/include/state.h"
 
 
+struct Quoridor_move : public MCTS_move {
+    short int x, y;
+    char player;
+    char type;         // 'h'/'v' -> horizontal/vertical wall, ' ' or other for move
+    Quoridor_move(short int x, short int y, char p, char t) : x(x), y(y), player(p), type(t) { }
+    bool operator==(const MCTS_move& other) const override {
+        const Quoridor_move &o = (const Quoridor_move &) other;
+        return x == o.x && y == o.y && player == o.player && type == o.type;
+    }
+    friend ostream& operator<<(ostream& os, Quoridor_move const &move) {
+        string movetype = (move.type == 'h') ? "horizontal wall" : (move.type == 'v') ? "vertical wall" : "move";
+        return os << move.player << " " << movetype << " " << ('a' + move.x) << move.y << endl;
+    }
+};
+
+
 class Quoridor_state : public MCTS_state {
     /** white's and black's coordinates on the board */
     short int wx, wy, bx, by;
     /** white's and black's remaining number of walls */
     short int wwallsno, bwallsno;
     /** Effectively a look-up table:
-     * 'h', 'v', 'b' for horizontal, vertical and both walls in the right and bottom part of a cell */
+     * ' ', 'h', 'v', 'b' for empty, horizontal, vertical and both walls in the right and bottom part of a cell */
     char walls[8][8]{};
     bool wall_connections[8][8]{};    // TODO: can be a bitmap to save up space (bools are 1 byte = 8 bits)
     /** Whose turn it is to play: 'W' or 'B' */
     char turn;
-
     //////////////////////////////////////////
     char change_turn() { turn = (turn == 'W') ? 'B' : 'W'; return turn; }
+    void add_wall(short int x, short int y, bool horizontal);
+    bool legal_step(short int x, short int y, char p) const;
+    bool legal_wall(short int x, short int y, char p, bool horizontal) const;
 public:
     Quoridor_state();
     Quoridor_state(const Quoridor_state &other);
-
+    char check_winner() const;
+    bool legal_move(const Quoridor_move *move) const;
+    void play_move(const Quoridor_move *move);
+    /** Overrides: */
+    bool is_terminal() const override;
+    MCTS_state *next_state(const MCTS_move *move) const override;
+    queue<MCTS_move *> *actions_to_try() const override;
+    double rollout() const override;                        // the rollout simulation in MCTS
+    void print() const override;
 };
 
 
